5c3c627e9456e3d8599a7aba98f46f297269db9d
Emmanuel Sarpong <83601745+emmanuelsarpong@users.noreply.github.com>
2024-07-11 09:17:48 -0400

    Implemented alert boxes

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
+  console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
@@ -12,17 +15,12 @@ const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      console.log('Login successful');
-      location.assign('/overview'); // Redirect to the homepage or any other page
+      showAlert('success', 'Login successful');
+      window.setTimeout(() => {
+        location.assign('/overview');
+      }, 1500);
     }
   } catch (err) {
-    console.error('Error:', err.response ? err.response.data : err.message);
+    showAlert('error', err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,79 +1,84 @@
 /* eslint-disable */
-document.addEventListener('DOMContentLoaded', () => {
-  const mapElement = document.getElementById('map');
-  if (mapElement) {
-    let locations = [];
-    try {
-      locations = JSON.parse(mapElement.dataset.locations);
-      console.log('Parsed locations:', locations);
-    } catch (error) {
-      console.error('Error parsing locations data:', error);
-      return;
-    }
 
-    if (typeof mapboxgl !== 'undefined') {
-      mapboxgl.accessToken =
-        'pk.eyJ1IjoiZW1tYW51ZWxzYXJwb25nIiwiYSI6ImNsd3FzbGFlNzA1cTUyanEwNHZ3eGdvc2EifQ._xg7sgSfctLjH3pG8bDS6Q';
-
-      const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/light-v10',
-      });
-
-      const bounds = new mapboxgl.LngLatBounds();
-
-      // Add markers to the map
-      locations.forEach((loc) => {
-        console.log('Processing location:', loc);
-
-        if (
-          loc.coordinates &&
-          Array.isArray(loc.coordinates) &&
-          loc.coordinates.length === 2
-        ) {
-          const [lng, lat] = loc.coordinates;
-          console.log('Coordinates:', lng, lat);
-
-          if (!isNaN(lng) && !isNaN(lat)) {
-            // Create marker
-            const el = document.createElement('div');
-            el.className = 'marker';
-
-            // Add marker
-            new mapboxgl.Marker({
-              element: el,
-              anchor: 'bottom',
-            })
-              .setLngLat([lng, lat])
-              .addTo(map);
-
-            console.log(`Added marker at [${lng}, ${lat}]`);
-
-            new mapboxgl.Popup()
-              .setLngLat(loc.coordinates)
-              .setHTML(`<p> ${loc.day}: ${loc.description}<o>`);
-
-            // Extend map bounds to include current location
-            bounds.extend([lng, lat]);
-          } else {
-            console.error('Invalid coordinates for location:', loc);
-          }
-        } else {
-          console.error('Invalid coordinates format for location:', loc);
-        }
-      });
-
-      // Check if bounds have been extended at least once
-      if (bounds.isEmpty()) {
-        console.error('Bounds have not been extended with valid coordinates.');
-      } else {
-        // Fit the map to the bounds
-        map.fitBounds(bounds, {
-          padding: { top: 200, bottom: 200, left: 100, right: 100 },
-        });
+export const displayMap = (locations) => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const mapElement = document.getElementById('map');
+    if (mapElement) {
+      let locations = [];
+      try {
+        locations = JSON.parse(mapElement.dataset.locations);
+        console.log('Parsed locations:', locations);
+      } catch (error) {
+        console.error('Error parsing locations data:', error);
+        return;
       }
-    } else {
-      console.error('Mapbox GL JS is not loaded.');
+
+      if (typeof mapboxgl !== 'undefined') {
+        mapboxgl.accessToken =
+          'pk.eyJ1IjoiZW1tYW51ZWxzYXJwb25nIiwiYSI6ImNsd3FzbGFlNzA1cTUyanEwNHZ3eGdvc2EifQ._xg7sgSfctLjH3pG8bDS6Q';
+
+        const map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/light-v10',
+        });
+
+        const bounds = new mapboxgl.LngLatBounds();
+
+        // Add markers to the map
+        locations.forEach((loc) => {
+          console.log('Processing location:', loc);
+
+          if (
+            loc.coordinates &&
+            Array.isArray(loc.coordinates) &&
+            loc.coordinates.length === 2
+          ) {
+            const [lng, lat] = loc.coordinates;
+            console.log('Coordinates:', lng, lat);
+
+            if (!isNaN(lng) && !isNaN(lat)) {
+              // Create marker
+              const el = document.createElement('div');
+              el.className = 'marker';
+
+              // Add marker
+              new mapboxgl.Marker({
+                element: el,
+                anchor: 'bottom',
+              })
+                .setLngLat([lng, lat])
+                .addTo(map);
+
+              console.log(`Added marker at [${lng}, ${lat}]`);
+
+              new mapboxgl.Popup()
+                .setLngLat(loc.coordinates)
+                .setHTML(`<p> ${loc.day}: ${loc.description}<o>`);
+
+              // Extend map bounds to include current location
+              bounds.extend([lng, lat]);
+            } else {
+              console.error('Invalid coordinates for location:', loc);
+            }
+          } else {
+            console.error('Invalid coordinates format for location:', loc);
+          }
+        });
+
+        // Check if bounds have been extended at least once
+        if (bounds.isEmpty()) {
+          console.error(
+            'Bounds have not been extended with valid coordinates.',
+          );
+        } else {
+          // Fit the map to the bounds
+          map.fitBounds(bounds, {
+            padding: { top: 200, bottom: 200, left: 100, right: 100 },
+          });
+        }
+      } else {
+        console.error('Mapbox GL JS is not loaded.');
+      }
     }
-  }
-});
+  });
+};
